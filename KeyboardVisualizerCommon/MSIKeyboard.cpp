6019718bd7ff35b448abbf3ef9442569d085f620
Adam Honse <calcprogrammer1@gmail.com>
2017-02-12 01:23:21 -0600

    Add keymap to map pixels to LED array, patterns now work, only issue is slow speed

--- KeyboardVisualizerCommon/MSIKeyboard.cpp
+++ KeyboardVisualizerCommon/MSIKeyboard.cpp
@@ -21,6 +21,39 @@ static unsigned int keys[] = {8,  9,  10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 21
                               106, 108, 109, 111, 112, 114, 115, 117, 118, 119, 120,
                               124, 128, 129 };
 
+static unsigned int keymap[6][23] =
+    { {   8,   0,  16,  24,  32,  40,   0,  48,  56,  64,  72,   0,  80,  88,  96, 104, 112, 120, 128,   0,   0,   0,   0 },
+      {   9,  17,  25,  33,  41,  49,  57,  65,  73,  81,  89,   0,  97, 105, 129,   0,  15,  31,  47,  63,  79,  95, 111 },
+      {  10,   0,  18,  26,  34,  42,   0,  50,  58,  66,  74,  82,  90,  98, 106, 114,  23,  39,  55,  71,  87, 103, 102 },
+      {  11,   0,  19,  27,  35,  43,   0,  51,  59,  67,  75,  83,  91,  99, 115,   0,   0,   0,   0,  46, 119,  78,   0 },
+      {  12,   0,  36,  44,  52,  60,   0,  68,   0,  76,  84,  92, 100, 108, 124,   0,   0,  38,   0,  54,  62,  86, 118 },
+      {  13,  21,  29,   0,   0,   0,   0,  45,   0,   0,   0,   0,  85,  93, 109, 117,  14,  22,  30,  70,   0,  94,   0 } };
+
+unsigned int keymap_idx[6][23];
+
+int PoseidonZXIndex[23];
+int PoseidonZYIndex[6];
+
+static void SetupKeyboardGrid(int x_count, int y_count, int * x_idx_list, int * y_idx_list)
+{
+    for (int x = 0; x < x_count; x++)
+    {
+        if (x < ((x_count) / 2))
+        {
+            x_idx_list[x] = (x * (SPECTROGRAPH_COLS / (x_count - 1))) + (0.5f * (SPECTROGRAPH_COLS / (x_count - 1)));
+        }
+        else
+        {
+            x_idx_list[x] = (x * (SPECTROGRAPH_COLS / (x_count - 1))) - (0.5f * (SPECTROGRAPH_COLS / (x_count - 1)));
+        }
+
+    }
+    for (int y = 0; y < y_count; y++)
+    {
+        y_idx_list[y] = ROW_IDX_SPECTROGRAPH_TOP + (y * (SPECTROGRAPH_ROWS / y_count)) + (0.5f * (SPECTROGRAPH_ROWS / y_count));
+    }
+}
+
 MSIKeyboard::MSIKeyboard()
 {
 }
@@ -43,6 +76,9 @@ void MSIKeyboard::Initialize()
     {
         init_ok = false;
     }
+
+    SetupKeyboardGrid(23, 6, PoseidonZXIndex, PoseidonZYIndex);
+
 }
 
 bool MSIKeyboard::SetLEDs(COLORREF pixels[64][256])
@@ -102,78 +138,54 @@ bool MSIKeyboard::SetLEDs(COLORREF pixels[64][256])
     usb.SendToDevice(buf, 8);
 #endif
 
-    unsigned char buf[264] = {0};
+    unsigned char red_buf[264];
+    unsigned char grn_buf[264];
+    unsigned char blu_buf[264];
 
-    //Send Red Packet
     for(int i = 0; i < 264; i++)
     {
-        buf[i] = 0xFF;
+        red_buf[i] = 0xFF;
+        grn_buf[i] = 0x00;
+        blu_buf[i] = 0x11;
     }
 
-    buf[0] = 0x07;
-    buf[1] = 0x09;
-    buf[2] = 0x01;
-    buf[3] = 0x01;
+    red_buf[0] = 0x07;
+    red_buf[1] = 0x09;
+    red_buf[2] = 0x01;
+    red_buf[3] = 0x01;
 
-    for(int i = 0; i < 104; i++)
+    grn_buf[0] = 0x07;
+    grn_buf[1] = 0x09;
+    grn_buf[2] = 0x01;
+    grn_buf[3] = 0x02;
+
+    blu_buf[0] = 0x07;
+    blu_buf[1] = 0x09;
+    blu_buf[2] = 0x01;
+    blu_buf[3] = 0x03;
+
+    for(int x = 0; x < 23; x++)
     {
-        buf[keys[i]] = GetRValue(pixels[ROW_IDX_SINGLE_COLOR][0]);;
+        for(int y = 0; y < 6; y++)
+        {
+            COLORREF color = pixels[PoseidonZYIndex[y]][PoseidonZXIndex[x]];
+            unsigned int idx = keymap[y][x];
+
+            if(idx != 0)
+            {
+                red_buf[idx] = GetRValue(color);
+                grn_buf[idx] = GetGValue(color);
+                blu_buf[idx] = GetBValue(color);
+            }
+        }
     }
 
-    usb.SendToDevice(buf, 264);
+    usb.SendToDevice(red_buf, 264);
     Sleep(10);
-    //Send Green Packet
-    for(int i = 0; i < 264; i++)
-    {
-        buf[i] = 0x00;
-    }
-
-    buf[0] = 0x07;
-    buf[1] = 0x09;
-    buf[2] = 0x01;
-    buf[3] = 0x02;
-
-    buf[8] = 0xFF;
-
-    for(int i = 0; i < 104; i++)
-    {
-        buf[keys[i]] = GetGValue(pixels[ROW_IDX_SINGLE_COLOR][0]);;
-    }
-
-    usb.SendToDevice(buf, 264);
+    usb.SendToDevice(grn_buf, 264);
     Sleep(10);
-    //Send Blue Packet
-    for(int i = 0; i < 264; i++)
-    {
-        buf[i] = 0x11;
-    }
+    usb.SendToDevice(blu_buf, 264);
+    Sleep(300);
 
-    buf[0] = 0x07;
-    buf[1] = 0x09;
-    buf[2] = 0x01;
-    buf[3] = 0x03;
-
-    for(int i = 0; i < 104; i++)
-    {
-       buf[keys[i]] = GetBValue(pixels[ROW_IDX_SINGLE_COLOR][0]);
-    }
-
-    usb.SendToDevice(buf, 264);
-    Sleep(10);
-    //Send Update Packet
-    for(int i = 0; i < 264; i++)
-    {
-        buf[i] = 0x00;
-    }
-
-    buf[0] = 0x07;
-    buf[1] = 0x02;
-    buf[2] = 0x01;
-    buf[8] = 0x01;  //Preset to save
-    buf[13] = 0x04; //Brightness?
-    buf[16] = 0x08;
-    buf[22] = 0x03;
-
-    //usb.SendToDevice(buf, 264);
     return init_ok;
 }
