95fcf6bb4956a927b305b01a336008e72344af14
Adam Honse <calcprogrammer1@gmail.com>
2017-03-10 03:45:10 +0000

    Update two sticks of RGB RAM and set correct color values from bar graph row

--- KeyboardVisualizerCommon/AsusAuraLinux.cpp
+++ KeyboardVisualizerCommon/AsusAuraLinux.cpp
@@ -40,15 +40,22 @@ bool AsusAura::SetLEDs(COLORREF pixels[64][256])
 
     for(int i = 0; i < 15; i+=3)
     {
-        smbus_led_data[i]   = (char)0xFF;
-        smbus_led_data[i+1] = (char)0xFF;
-        smbus_led_data[i+2] = (char)0xFF;
+        COLORREF color = pixels[ROW_IDX_BAR_GRAPH][255 - i * (128 / 15)];
+        smbus_led_data[i]   = (char)GetRValue(color);
+        smbus_led_data[i+1] = (char)GetBValue(color);
+        smbus_led_data[i+2] = (char)GetGValue(color);
     }
 
+    ioctl(smbus_fd, I2C_SLAVE, 0x72);
     i2c_smbus_write_word_data(smbus_fd, 0x00, 0x0080);
     i2c_smbus_write_block_data(smbus_fd, 0x03, 15, (__u8*)&smbus_led_data);
     i2c_smbus_write_word_data(smbus_fd, 0x00, 0xA080);
     i2c_smbus_write_byte_data(smbus_fd, 0x01, 0x01);
 
+    ioctl(smbus_fd, I2C_SLAVE, 0x73);
+    i2c_smbus_write_word_data(smbus_fd, 0x00, 0x0080);
+    i2c_smbus_write_block_data(smbus_fd, 0x03, 15, (__u8*)&smbus_led_data);
+    i2c_smbus_write_word_data(smbus_fd, 0x00, 0xA080);
+    i2c_smbus_write_byte_data(smbus_fd, 0x01, 0x01);
     return true;
 }
