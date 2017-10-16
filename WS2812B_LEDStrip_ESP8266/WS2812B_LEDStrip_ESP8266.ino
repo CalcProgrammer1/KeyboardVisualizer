22c37ef7801222d1f647cf034a9f782c276a8548
Adam Honse <calcprogrammer1@gmail.com>
2017-10-16 13:20:27 -0500

    Update WS2812B_LEDStrip_ESP8266.ino

--- WS2812B_LEDStrip_ESP8266/WS2812B_LEDStrip_ESP8266.ino
+++ WS2812B_LEDStrip_ESP8266/WS2812B_LEDStrip_ESP8266.ino
@@ -19,9 +19,11 @@ WiFiUDP Udp;
 
 char packet_buf[1024];
 
-#define PIN 2
-#define LEDS 30
-#define PACKET_SZ ( (LEDS * 3) + 3 )
+#define LEDS_PER_STRIP 50
+#define STRIPS 4
+#define PACKET_SZ ( (LEDS_PER_STRIP * STRIPS * 3) + 3 )
+
+const int pins[STRIPS] = { 0, 2, 3, 4 };
 
 // Parameter 1 = number of pixels in strip
 // Parameter 2 = Arduino pin number (most are valid)
@@ -30,10 +32,15 @@ char packet_buf[1024];
 //   NEO_KHZ400  400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
 //   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
 //   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(LEDS, PIN, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel strips[STRIPS];
 
 void setup()
 {
+  for( int i = 0; i < STRIPS; i++ )
+  {
+    strips[i] = Adafruit_NeoPixel(LEDS, pins[i], NEO_GRB + NEO_KHZ800);
+  }
+  
   Serial.begin(115200);
   delay(10);
 
@@ -85,13 +92,16 @@ void loop()
       //Test if valid write packet
       if( ( ( (unsigned short)packet_buf[checksum_0] << 8 ) | packet_buf[checksum_1] ) == sum )
       {
-        for( int i = 0; i < LEDS; i++ )
+        for( int i = 0; i < STRIPS; i++ )
         {
-          int idx = 1 + ( 3 * i );
+          for( int j = 0; j < LEDS_PER_STRIP; j++ )
+          {
+          int idx = 1 + ( i * STRIPS ) + ( 3 * j );
           
-          strip.setPixelColor(i, strip.Color(packet_buf[idx], packet_buf[idx+1], packet_buf[idx+2]));
+          strips[i].setPixelColor(j, strips[i].Color(packet_buf[idx], packet_buf[idx+1], packet_buf[idx+2]));
+          }
+        strips[i].show();
         }
-        strip.show();
       }
     }
   }
